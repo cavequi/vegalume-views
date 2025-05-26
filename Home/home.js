94dba3b585b8c6aa6a89239ba68b33c4700b3288
Vinicius <vinicavequi@gmail.com>
2025-05-26 02:53:57 -0300

    terminando a view Home

--- Home/home.js
+++ Home/home.js
@@ -1,14 +1,29 @@
-function revelaEsconde(conteudo){
-    if (conteudo === "localizacao"){
+function revelaEsconde(conteudo) {
+    if (conteudo === "localizacao") {
         document.getElementById("p-localizacao").classList.toggle("mostrar");
         document.getElementById("btn-localizacao").classList.toggle("menos");
     }
-    if (conteudo === "horarios"){
+    if (conteudo === "horarios") {
         document.getElementById("p-horarios").classList.toggle("mostrar");
         document.getElementById("btn-horarios").classList.toggle("menos");
     }
-    if (conteudo === "contatos"){
+    if (conteudo === "contatos") {
         document.getElementById("p-contatos").classList.toggle("mostrar");
         document.getElementById("btn-contatos").classList.toggle("menos");
     }
 }
+
+const btn = document.getElementById('scrollTop');
+
+/*window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        btn.classList.add('show');
+        alert("hi");
+    } else {
+        btn.classList.remove('show');
+    }
+});*/
+
+btn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
